dde03d59e99347f2300d7fa1a44109c7934f75f0
Joshua Wiser <Jwiser53@byui.edu>
2023-09-21 20:46:49 -0600

    all done but the photo wont pull up idk why

---
+++ scripts/w02-task.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Step 2 - Variables
+    const fullName = 'Joshua Wiser';
+    const currentYear = new Date().getFullYear(); // Get the current year dynamically
+    const profilePicture = "Fall 23\cse121b\images\photo_of_me.jpg"; // Assuming the image is in the same directory as your HTML file
+
+    // Step 3 - Element Variables
+    const nameElement = document.getElementById('name');
+    const foodElement = document.getElementById('food');
+    const yearElement = document.getElementById('year');
+
+    // Check if the nameElement, foodElement, and yearElement exist
+    if (nameElement && foodElement && yearElement) {
+        // Step 4 - Adding Content
+        nameElement.innerHTML = `<strong>${fullName}</strong>`;
+        yearElement.textContent = `<strong>${currentYear}</strong>`;
+
+        // Check if the imageElement (profilePicture) exists
+        const imageElement = document.querySelector('img');
+        if (imageElement) {
+            imageElement.src = profilePicture;
+            imageElement.alt = `Profile image of ${fullName}`;
+        } else {
+            console.error("The 'profilePicture' element (img) was not found.");
+        }
+
+        // Step 5 - Array
+        const favoriteFoods = ["Pizza", "Sushi", "Chocolate"];
+        foodElement.innerHTML = favoriteFoods.join('<br>');
+
+        const newFavoriteFood = "Burgers";
+        favoriteFoods.push(newFavoriteFood);
+        foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+
+        favoriteFoods.shift();
+        foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+
+        favoriteFoods.pop();
+        foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+    } else {
+        console.error("One or more required elements were not found.");
+    }
+});
